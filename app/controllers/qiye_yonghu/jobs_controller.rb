10f2ba6f2f28d239fa6a2941db87edce171a8769
zee <kooo9671354@gmail.com>
2022-09-21 16:36:28 +0800

    implement qiye_yonghu/jobs controller

--- app/controllers/qiye_yonghu/jobs_controller.rb
+++ app/controllers/qiye_yonghu/jobs_controller.rb
@@ -5,7 +5,49 @@ class QiyeYonghu::JobsController < ApplicationController
     layout 'qiye_yonghu'
 
     def index
-      @jobs = Job.all
+      @jobs = current_user.jobs
+    end
+
+    def new
+      @job = Job.new
+    end
+
+    def create
+      @job= Job.new(job_params)
+      @job.user = current_user
+      if @job.save
+        redirect_to qiye_yonghu_jobs_path
+      else
+        back_url
+      end
+    end
+
+    def edit
+      @jobs = current_user.jobs
+      @job = @jobs.find_by(params[:user_id])
+    end
+
+    def update
+      @jobs = current_user.jobs
+      @job = @jobs.find_by(params[:user_id])
+      if @job.update(job_params)
+        redirect_to qiye_yonghu_jobs_path
+      else
+        back_url
+      end
+    end
+
+    def destroy
+      @jobs = current_user.jobs
+      @job = @jobs.find_by(params[:user_id])
+      @job.destroy
+    end
+
+    private
+
+    def job_params
+      params.require(:jobs).permit(:user_id, :job_name, :job_miaoshu, :job_price, :lianxi_fangshi,
+         :job_action, :job_address)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :skills
+         has_many :jobs
 
          def user_first_touch_skill
            self.user_first_touch = true
